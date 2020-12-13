b1ff10d477b3c9d71e5aeec2d0af55238b78414b
Maarten Mortier <maarten.mortier@gmail.com>
2020-12-13 17:49:33 +0100

    Add Day 13

---
+++ day13.jl
@@ -0,0 +1,112 @@
+function part1()
+    lines = readlines("day13.txt")
+    ts = parse(Int, lines[1])
+    buses = split(lines[2],",")
+
+    minimum = 0
+    minimumBus = 0
+
+    for b in buses
+        if b == "x"
+            continue
+        end
+
+        b = parse(Int,b)
+
+        dt = Int(b*(floor(ts/b)+1) - ts)
+
+        if (minimum == 0 || dt < minimum)
+            minimum = dt
+            minimumBus = b
+        end
+    end
+
+    return minimumBus * minimum
+end
+
+function pb(bus)
+    if (bus == "x")
+        return 0
+    end
+end
+
+function search(start,diff,multiplier, next, buses)
+    candidate = start
+
+    if (next > length(buses) || buses[next][2] == "x")
+        return true
+    end
+
+    while true
+        nt = buses[next][1]
+        nb = parse(Int, buses[next][2])
+        nd = nt - diff - 1
+
+        print("Trying to find next number ", candidate, " by checking if ", candidate + nd, " divides ", nb, "\n")
+
+        if (candidate > 4000) # early stop
+            return false
+        end
+
+        if mod(nb, candidate + nd) == 0 # possible candidate
+            if search(candidate, buses[next][1] - 1, nb, next + 1, buses[next:end])
+                return true
+            end
+        else
+            candidate += multiplier
+            continue
+        end
+
+      
+    end
+
+    return false
+end
+
+function part2()
+    lines = readlines("day13.txt")
+    _ = parse(Int, lines[1])
+    buses = [x for x in enumerate(split(lines[2],","))]
+
+    t = 0
+    multiplier = 1
+
+    while true
+        found = true
+        for bus in buses
+            if (bus[2] == "x")
+                continue
+            end
+
+            bt = Int128(t + bus[1] - 1)
+            b = parse(Int, bus[2])
+
+            #print("Checking if ", b, " divides ", bt, ": ", mod(bt, b) ,"\n")
+
+            if (mod(bt, b) != 0)
+                found = false
+                break
+            else
+                if (mod(multiplier, b) != 0)
+                    multiplier *= b # jump to the next point where these align
+                end
+            end
+        end
+
+        if found
+            return t
+        end
+
+        t += multiplier
+
+        if (t > 10000000000000000000)
+            print("Early exit to prevent inf loop: ")
+            return t
+        end
+    end
+end
+
+
+print(part1())
+print("\n")
+print(part2())
