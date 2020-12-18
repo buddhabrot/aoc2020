414054545bfc2df1844523a8b1903801403d52d9
Maarten Mortier <maarten.mortier@gmail.com>
2020-12-18 09:26:56 +0100

    added day 2

--- day18.jl
+++ day18.jl
@@ -8,4 +8,14 @@ function part1()
     sum(map(s->eval(Meta.parse(s)), lines))
 end
 
+
+function part2()
+    global lines
+    ^(a,b) = a+b # define addition as being higher precedence 
+    lines = map(s->replace(s,"+" => "^"), lines)
+    sum(map(s->eval(Meta.parse(s)), lines))
+end
+
 part1()
+print("\n")
+part2()
